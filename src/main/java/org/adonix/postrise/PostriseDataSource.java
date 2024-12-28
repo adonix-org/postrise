c82a5d3f147a7dacdd822f2e0381707378b02b1a
Ty Busby <tybusby@gmail.com>
2024-12-28 17:14:50 -0500

    Rename local variable.

--- src/main/java/org/adonix/postrise/PostriseDataSource.java
+++ src/main/java/org/adonix/postrise/PostriseDataSource.java
@@ -6,166 +6,166 @@ import com.zaxxer.hikari.HikariDataSource;
 
 abstract class PostriseDataSource implements ConnectionProvider {
 
-    private final HikariDataSource dataSource = new HikariDataSource();
+    private final HikariDataSource delegate = new HikariDataSource();
 
     @Override
     public Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
+        return delegate.getConnection();
     }
 
     @Override
     public void setJdbcUrl(final String url) {
-        dataSource.setJdbcUrl(url);
+        delegate.setJdbcUrl(url);
     }
 
     @Override
     public String getJdbcUrl() {
-        return dataSource.getJdbcUrl();
+        return delegate.getJdbcUrl();
     }
 
     @Override
     public void setDriverClassName(final String driverClassName) {
-        dataSource.setDriverClassName(driverClassName);
+        delegate.setDriverClassName(driverClassName);
     }
 
     @Override
     public String getDriverClassName() {
-        return dataSource.getDriverClassName();
+        return delegate.getDriverClassName();
     }
 
     @Override
     public void setUsername(final String username) {
-        dataSource.setUsername(username);
+        delegate.setUsername(username);
     }
 
     @Override
     public String getUsername() {
-        return dataSource.getUsername();
+        return delegate.getUsername();
     }
 
     @Override
     public void setPassword(final String password) {
-        dataSource.setPassword(password);
+        delegate.setPassword(password);
     }
 
     @Override
     public String getPassword() {
-        return dataSource.getPassword();
+        return delegate.getPassword();
     }
 
     @Override
     public void setMaxPoolSize(final int size) {
-        dataSource.setMaximumPoolSize(size);
+        delegate.setMaximumPoolSize(size);
     }
 
     @Override
     public int getMaxPoolSize() {
-        return dataSource.getMaximumPoolSize();
+        return delegate.getMaximumPoolSize();
     }
 
     @Override
     public void setConnectionTimeout(final long connectionTimeoutMs) {
-        dataSource.setConnectionTimeout(connectionTimeoutMs);
+        delegate.setConnectionTimeout(connectionTimeoutMs);
     }
 
     @Override
     public long getConnectionTimeout() {
-        return dataSource.getConnectionTimeout();
+        return delegate.getConnectionTimeout();
     }
 
     @Override
     public void setIdleTimeout(final long idleTimeoutMs) {
-        dataSource.setIdleTimeout(idleTimeoutMs);
+        delegate.setIdleTimeout(idleTimeoutMs);
     }
 
     @Override
     public long getIdleTimeout() {
-        return dataSource.getIdleTimeout();
+        return delegate.getIdleTimeout();
     }
 
     @Override
     public void setMinIdle(final int minIdle) {
-        dataSource.setMinimumIdle(minIdle);
+        delegate.setMinimumIdle(minIdle);
     }
 
     @Override
     public int getMinIdle() {
-        return dataSource.getMinimumIdle();
+        return delegate.getMinimumIdle();
     }
 
     @Override
     public void setAutoCommit(final boolean isAutoCommit) {
-        dataSource.setAutoCommit(isAutoCommit);
+        delegate.setAutoCommit(isAutoCommit);
     }
 
     @Override
     public boolean isAutoCommit() {
-        return dataSource.isAutoCommit();
+        return delegate.isAutoCommit();
     }
 
     @Override
     public void setValidationTimeout(final long validationTimeoutMs) {
-        dataSource.setValidationTimeout(validationTimeoutMs);
+        delegate.setValidationTimeout(validationTimeoutMs);
     }
 
     @Override
     public long getValidationTimeout() {
-        return dataSource.getValidationTimeout();
+        return delegate.getValidationTimeout();
     }
 
     @Override
     public void setLeakDetectionThreshold(final long leakDetectionThresholdMs) {
-        dataSource.setLeakDetectionThreshold(leakDetectionThresholdMs);
+        delegate.setLeakDetectionThreshold(leakDetectionThresholdMs);
     }
 
     @Override
     public long getLeakDetectionThreshold() {
-        return dataSource.getLeakDetectionThreshold();
+        return delegate.getLeakDetectionThreshold();
     }
 
     @Override
     public void setMaxLifetime(final long maxLifetimeMs) {
-        dataSource.setMaxLifetime(maxLifetimeMs);
+        delegate.setMaxLifetime(maxLifetimeMs);
     }
 
     @Override
     public long getMaxLifetime() {
-        return dataSource.getMaxLifetime();
+        return delegate.getMaxLifetime();
     }
 
     @Override
     public void setConnectionTestQuery(final String sql) {
-        dataSource.setConnectionTestQuery(sql);
+        delegate.setConnectionTestQuery(sql);
     }
 
     @Override
     public String getConnectionTestQuery() {
-        return dataSource.getConnectionTestQuery();
+        return delegate.getConnectionTestQuery();
     }
 
     @Override
     public void addDataSourceProperty(String propertyName, Object value) {
-        dataSource.addDataSourceProperty(propertyName, value);
+        delegate.addDataSourceProperty(propertyName, value);
     }
 
     @Override
     public int getActiveConnections() {
-        return dataSource.getHikariPoolMXBean().getActiveConnections();
+        return delegate.getHikariPoolMXBean().getActiveConnections();
     }
 
     @Override
     public int getIdleConnections() {
-        return dataSource.getHikariPoolMXBean().getIdleConnections();
+        return delegate.getHikariPoolMXBean().getIdleConnections();
     }
 
     @Override
     public int getTotalConnections() {
-        return dataSource.getHikariPoolMXBean().getTotalConnections();
+        return delegate.getHikariPoolMXBean().getTotalConnections();
     }
 
     @Override
     public void close() {
-        dataSource.close();
+        delegate.close();
     }
 
     @Override
