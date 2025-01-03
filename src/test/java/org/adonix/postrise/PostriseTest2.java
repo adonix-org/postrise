5554ee72fbca8d8d499f14cc1af40c1acd966b55
Ty Busby <tybusby@gmail.com>
2025-01-02 21:55:01 -0500

    Refactor testing.

--- src/test/java/org/adonix/postrise/PostriseTest2.java
+++ src/test/java/org/adonix/postrise/PostriseTest2.java
@@ -1,39 +1,40 @@
 package org.adonix.postrise;
 
 import java.sql.Connection;
+
 import org.adonix.postrise.servers.Servers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-public class PostriseTest extends TestEnvironment {
+public class PostriseTest2 extends TestEnvironment {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Test
-    public void run() throws Exception {
+    public void run4() throws Exception {
         try (final Server server = Servers.getLocalhostSuper();
                 final Connection connection = server.getConnection("test", "test")) {
             connection.setAutoCommit(false);
-            LOGGER.info("run");
+            LOGGER.info("run4");
         }
     }
 
     @Test
-    public void run2() throws Exception {
+    public void run5() throws Exception {
         try (final Server server = Servers.getLocalhostSuper();
                 final Connection connection = server.getConnection("test", "test")) {
             connection.setAutoCommit(false);
-            LOGGER.info("run2");
+            LOGGER.info("run5");
         }
     }
 
     @Test
-    public void run3() throws Exception {
+    public void run6() throws Exception {
         try (final Server server = Servers.getLocalhostSuper();
                 final Connection connection = server.getConnection("test", "test")) {
             connection.setAutoCommit(false);
-            LOGGER.info("run3");
+            LOGGER.info("run6");
         }
     }
 }
