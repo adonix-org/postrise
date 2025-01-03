c29ae84f8cd407e03aacbc6a765357c0e72c7451
Ty Busby <tybusby@gmail.com>
2025-01-03 18:47:44 -0500

    Add final keyword to parameters.

--- src/test/java/org/adonix/postrise/SuperUserAccess.java
+++ src/test/java/org/adonix/postrise/SuperUserAccess.java
@@ -3,7 +3,7 @@ package org.adonix.postrise;
 public class SuperUserAccess extends PostgresTestServer implements DataSourceListener {
 
     @Override
-    public void onConfigure(ConnectionSettings settings) {
+    public void onConfigure(final ConnectionSettings settings) {
         settings.setJdbcUrl(container.withDatabaseName(settings.getDatabase()).getJdbcUrl());
         settings.setUsername(container.getUsername());
         settings.setPassword(container.getPassword());
