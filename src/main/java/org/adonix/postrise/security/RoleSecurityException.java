5df76cf52ad175180d1573da3fce13c3ad6073ca
Ty Busby <tybusby@gmail.com>
2025-01-21 02:17:12 -0500

    Renamed private method.

--- src/main/java/org/adonix/postrise/security/RoleSecurityException.java
+++ src/main/java/org/adonix/postrise/security/RoleSecurityException.java
@@ -4,11 +4,11 @@ import java.sql.SQLException;
 
 public class RoleSecurityException extends SQLException {
 
-    private static final String SQL_STATE_INVALID_ROLE = "28000";
+    private static final String SQL_STATE_INSECURE_ROLE = "28000";
 
     private static final String MESSAGE_PREFIX = "ROLE SECURITY: ";
 
     protected RoleSecurityException(final String message) {
-        super(MESSAGE_PREFIX + message.trim(), SQL_STATE_INVALID_ROLE);
+        super(MESSAGE_PREFIX + message.trim(), SQL_STATE_INSECURE_ROLE);
     }
 }
