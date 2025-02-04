4600a45c0c392a564e4285231516a9cbf550f6c5
Ty Busby <tybusby@gmail.com>
2025-02-04 13:12:08 -0500

    Clean up

--- src/main/java/org/adonix/postrise/PostriseServer.java
+++ src/main/java/org/adonix/postrise/PostriseServer.java
@@ -29,7 +29,6 @@ import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
