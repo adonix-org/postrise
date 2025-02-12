a115e1c7e183559c792ec4c308a75c507165b66a
Ty Busby <tybusby@gmail.com>
2025-02-11 23:33:06 -0500

    Remote unused json.org and update to latest version in pom.xml.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -19,7 +19,6 @@ module org.adonix.postrise {
     requires com.zaxxer.hikari;
     requires transitive java.sql;
     requires org.apache.logging.log4j;
-    requires transitive org.json;
 
     exports org.adonix.postrise;
     exports org.adonix.postrise.security;
