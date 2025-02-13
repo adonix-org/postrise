460d1d3dc3277dd872ee8e4c17ac0524fde28ace
Ty Busby <tybusby@gmail.com>
2025-02-12 23:49:01 -0500

    Renamed TEST class.

--- src/test/java/org/adonix/postrise/TestBasicOperation.java
+++ src/test/java/org/adonix/postrise/TestBasicOperation.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TestStandardOperation {
+public class TestBasicOperation {
 
     private static final PostgresDocker server = new TestServer();
 
