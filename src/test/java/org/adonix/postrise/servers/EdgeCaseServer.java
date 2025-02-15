4e117e914c86079b0e0fe01d655c0f1aa757894c
Ty Busby <tybusby@gmail.com>
2025-02-14 20:28:54 -0500

    Remove unused beforeCreate()

--- src/test/java/org/adonix/postrise/servers/EdgeCaseServer.java
+++ src/test/java/org/adonix/postrise/servers/EdgeCaseServer.java
@@ -16,15 +16,9 @@
 
 package org.adonix.postrise.servers;
 
-import org.adonix.postrise.DataSourceSettings;
 import org.adonix.postrise.PostgresServer;
 
 public class EdgeCaseServer extends PostgresServer {
-
-    @Override
-    public void beforeCreate(final DataSourceSettings settings) {
-        super.beforeCreate(settings);
-    }
 
     @Override
     protected void beforeClose() {
